42239cd14ae0e84d6b32bfd94210521e7d470b22
aleksandr-b87 <74981378+aleksandr-b87@users.noreply.github.com>
2020-11-24 20:25:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import *
+import os
 
 app = Flask(__name__)
 
@@ -79,4 +80,7 @@ def form_sample():
         return "Форма отправлена"
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    #app.run(port=8080, host='127.0.0.1')
+    #app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)    
